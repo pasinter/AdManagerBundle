48c21964575dc8ae602d9b8e0992bc1301602c92
kgolovin <ken.golovin@honda.co.nz>
2013-01-16 16:21:48 +1300

    added admanager_campaign_render_one() function

--- Twig/CampaignExtension.php
+++ Twig/CampaignExtension.php
@@ -31,6 +31,8 @@ class CampaignExtension extends \Twig_Extension implements ContainerAwareInterfa
         return array(
             'admanager_campaign_get' => new \Twig_Function_Method($this, 'get'),
             'admanager_campaign_render' => new \Twig_Function_Method($this, 'render', array('is_safe' => array('html'))),
+            
+            'admanager_campaign_render_one' => new \Twig_Function_Method($this, 'renderOne', array('is_safe' => array('html'))),
         );
     }
 
@@ -94,6 +96,32 @@ class CampaignExtension extends \Twig_Extension implements ContainerAwareInterfa
         }
         
         return '';
+    }
+    
+    public function renderOne($code, array $options = array(), $renderer = null)
+    {
+        $campaign = $this->get($code, $options);
+
+        if($campaign) {
+            $html = '<div class="admanaget_render_one" style="width:728px; height: 90px;overflow: hidden;">';
+
+            $ad = $campaign->getAds()->first();
+            $media = $ad->getImage();
+
+            $mediaservice = $this->container->get('sonata.media.pool');
+            $provider = $mediaservice->getProvider($media->getProviderName());
+            $format = $provider->getFormatName($media, 'big');
+
+            $html .= '<a href="' . $ad->getLink() . '">';
+            $html .= '<img src="' . $provider->generatePublicUrl($media, $format) .  '" width="728" height="90">';
+            $html .= '</a>';
+
+            $html .= '</div>';
+            
+            return $html;
+        }
+        
+        return '<!-- admanager_campaign_render_one: 0 results -- >';
     }
 
     /**
